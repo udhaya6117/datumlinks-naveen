f93a2ca5224b61d8ebebc0912c6ad5fdd5b14214
udhaya6117 <udhayaprakash6117@gmail.com>
2024-01-13 14:56:49 +0530

    added

--- src/components/Landingpageheader.js
+++ src/components/Landingpageheader.js
@@ -1,40 +1,51 @@
-import React from 'react'
-import logo from "../img/logo.png"
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from "react";
+import logo from "../img/logo.png";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Landingpageheader = () => {
   return (
-    // <div className='header'>
-    //   <div className='logo'>
-    //        <img src={logo} alt='logo' />
-    //   </div>
-    //   <div></div>
-    //   <div></div>
-    // </div>
-    <AppBar position="static">
-    <Toolbar>
-      <IconButton
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="menu"
-        sx={{ mr: 2 }}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        News
-      </Typography>
-      <Button color="inherit">Login</Button>
-    </Toolbar>
-  </AppBar>
-  )
-}
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+    >
+      <Toolbar>
+        <div className="header-split">
+          <Typography variant="h6" component="div" className="logo">
+            <img src={logo} alt="logo" />
+          </Typography>
+          <div className="header-navigations">
+            <div>
+              <text>Home</text>
+            </div>
+            <div>
+              <text>About</text>
+            </div>
+            <div>
+              <text>Services</text>
+            </div>
+            <div>
+              <text>Work</text>
+            </div>
+            <div>
+              <text>Contact</text>
+            </div>
+            <div>
+              <text>Resources</text>
+            </div>
+          </div>
+          <div className="btn-for-quote">
+            <button>Get a quote</button>
+          </div>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Landingpageheader
+export default Landingpageheader;

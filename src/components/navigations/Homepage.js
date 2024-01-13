bc0fe5cb79be037add26a63ed2e2c59aa7d52115
udhaya <udhayaprakash6117@gmail.com>
2024-01-14 00:28:08 +0530

    hlaf postion is done

--- src/components/navigations/Homepage.js
+++ src/components/navigations/Homepage.js
@@ -6,16 +6,20 @@ import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import IconButton from "@mui/material/IconButton";
 import dotparagrarf from "../../img/hemopage-imgs/Frame 29.png";
 import { styled } from "@mui/material/styles";
-import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import MuiAccordion from "@mui/material/Accordion";
 import MuiAccordionSummary from "@mui/material/AccordionSummary";
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
+import product2 from "../../img/hemopage-imgs/product-2.png"
+import product3 from "../../img/hemopage-imgs/product-3.png"
+import client from "../../img/hemopage-imgs/client-img.png"
 
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
   backgroundColor: "#F8F8F8",
+  padding:"10px",
   borderRadius: "5px", // Adjust the value as needed
   "&:not(:last-child)": {
     borderBottom: 0,
@@ -27,12 +31,12 @@ const Accordion = styled((props) => (
 
 const AccordionSummary = styled((props) => (
   <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    expandIcon={<KeyboardArrowDownIcon sx={{ fontSize: "0.9rem" }} />}
     {...props}
   />
 ))(({ theme }) => ({
   "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
-    transform: "rotate(90deg)",
+    transform: "rotate(180deg)",
   },
 }));
 
@@ -172,7 +176,180 @@ const Homepage = () => {
               </div>
             </div>
           </div>
+          <div className="services-homes">
+            <div className="products-homes">
+              <text>02</text>
+              <h1>Web Design & Development </h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                aliquam erat volutpat.{" "}
+              </p>
+            </div>
+            <div className="products-homes-detatils">
+              <div className="products-imgs">
+                <img src={product2} alt="img" />
+              </div>
+              <div className="accordion">
+                <Accordion
+                  expanded={expanded === "panel1"}
+                  onChange={handleChange("panel1")}
+                >
+                  <AccordionSummary
+                    aria-controls="panel1d-content"
+                    id="panel1d-header"
+                  >
+                    <Typography className="header-for-AccordionSummary">
+                    Creative Logo Design 
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography className="details-for-AccordionSummary">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel2"}
+                  onChange={handleChange("panel2")}
+                >
+                  <AccordionSummary
+                    aria-controls="panel2d-content"
+                    id="panel2d-header"
+                  >
+                    <Typography className="header-for-AccordionSummary">Marketing Collateral </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography className="details-for-AccordionSummary">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel3"}
+                  onChange={handleChange("panel3")}
+                >
+                  <AccordionSummary
+                    aria-controls="panel3d-content"
+                    id="panel3d-header"
+                  >
+                    <Typography className="header-for-AccordionSummary">Graphic Design </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography className="details-for-AccordionSummary">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel4"}
+                  onChange={handleChange("panel4")}
+                >
+                  <AccordionSummary
+                    aria-controls="panel4d-content"
+                    id="panel4d-header"
+                  >
+                    <Typography className="header-for-AccordionSummary">Social Media Design </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography className="details-for-AccordionSummary">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+            </div>
+          </div>
+          <div className="services-homes">
+            <div className="products-homes">
+              <text>03</text>
+              <h1>Digital Marketing </h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                aliquam erat volutpat.{" "}
+              </p>
+            </div>
+            <div className="products-homes-detatils">
+              <div className="products-imgs">
+                <img src={product3} alt="img" />
+              </div>
+              <div className="accordion">
+                <Accordion
+                  expanded={expanded === "panel1"}
+                  onChange={handleChange("panel1")}
+                >
+                  <AccordionSummary
+                    aria-controls="panel1d-content"
+                    id="panel1d-header"
+                  >
+                    <Typography className="header-for-AccordionSummary">
+                    Creative Logo Design 
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography className="details-for-AccordionSummary">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel2"}
+                  onChange={handleChange("panel2")}
+                >
+                  <AccordionSummary
+                    aria-controls="panel2d-content"
+                    id="panel2d-header"
+                  >
+                    <Typography className="header-for-AccordionSummary">Marketing Collateral </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography className="details-for-AccordionSummary">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel3"}
+                  onChange={handleChange("panel3")}
+                >
+                  <AccordionSummary
+                    aria-controls="panel3d-content"
+                    id="panel3d-header"
+                  >
+                    <Typography className="header-for-AccordionSummary">Graphic Design </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography className="details-for-AccordionSummary">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion
+                  expanded={expanded === "panel4"}
+                  onChange={handleChange("panel4")}
+                >
+                  <AccordionSummary
+                    aria-controls="panel4d-content"
+                    id="panel4d-header"
+                  >
+                    <Typography className="header-for-AccordionSummary">Social Media Design </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography className="details-for-AccordionSummary">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+            </div>
+          </div>
         </Container>
+      </div>
+
+      <div className="client-header">
+        <div className="title-client"><h2>Our Happy Clients</h2><img src={client} alt="img" /></div>
+         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore </p>
       </div>
     </div>
   );
